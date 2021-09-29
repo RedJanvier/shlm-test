eda3a6b7d2d27fb6d26e5f5ecd4b284ef85b4841
redjanvier <janvierntwali@gmail.com>
2021-09-29 14:09:31 +0200

    feat(filter): by animals name naive approach

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const { data } = require('./data');
 const count = process.argv;
 
-if (count.includes('--count')) {
+const isCount = count.includes('--count');
+const isFilter = count.findIndex((v) => v.includes('--filter='));
+
+if (isCount && isFilter < 0) {
   const toDisplay = data.map((x) => {
     return {
       name: `${x.name} [${x.people.length}]`,
@@ -14,4 +17,28 @@ if (count.includes('--count')) {
     };
   });
   console.log(JSON.stringify(toDisplay));
+} else if (isFilter >= 0 && !isCount) {
+  const query = count[isFilter].split('=')[1];
+  const toDisplay = data
+    .map((x) => {
+      const country = {
+        name: x.name,
+        people: x.people
+          .map((y) => {
+            const person = {
+              name: y.name,
+              animals: y.animals.filter((z) => z.name.includes(query)),
+            };
+            return person.animals.length ? person : false;
+          })
+          .filter((x) => x),
+      };
+      return country.people.length ? country : false;
+    })
+    .filter((x) => x);
+  console.log(JSON.stringify(toDisplay));
+} else {
+  console.log('isCount: ', isCount);
+  console.log('isFilter: ', isFilter);
+  console.log('Unkown command provided!');
 }
